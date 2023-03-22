21eb211de5e5fcf3aedf5b5457bd58faf168d4c1
Weiyan  Hu <austinhu0802@gmail.com>
2023-03-22 19:08:15 +0800

    first commit

---
+++ src/test/java/com/example/jenkins_web_demo/JenkinsWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_web_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

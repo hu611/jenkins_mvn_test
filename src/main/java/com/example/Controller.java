e0938b2a5364a01c2c41ed6bef7f12ff657f6ea6
Weiyan Hu <43009969+hu611@users.noreply.github.com>
2023-03-23 18:34:51 +0800

    Update Controller.java

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    //lol
+    //lollll
     @GetMapping("/")
     public String welcome() {
         return "welcome";

1113d96259af07495c174bf225c0b71c9e00a189
Weiyan  Hu <austinhu0802@gmail.com>
2023-03-23 16:37:02 +0800

    added controller

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,2 +1,12 @@
-package com.example;public class Controller {
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
 }
